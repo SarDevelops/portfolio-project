67897bdc38488ad4ff0aebc1aad7a1678437532f
sahildevelops <sahildevelops99@gmail.com>
2025-02-22 18:08:14 +0530

    added the theme

--- resources/js/Pages/Blogs/Blog.vue
+++ resources/js/Pages/Blogs/Blog.vue
@@ -4,16 +4,14 @@ import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <Head title="Blogs" />
+  <Head title="Blogs" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                Blogs
-            </h2>
-        </template>
-    </AuthenticatedLayout>
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        Blogs
+      </h2>
+    </template>
+  </AuthenticatedLayout>
 </template>
 <style lang="css"></style>
